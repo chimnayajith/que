5a0963c1d87a4e1e76d229c3b9609aea58f81aea
chimnayajith <chinmayajith30@gmail.com>
2026-04-17 22:07:00 +0530

    Refactor QueueService to manage QueueUser instances with a PriorityQueue

--- src/main/java/com/que/que/service/QueueService.java
+++ src/main/java/com/que/que/service/QueueService.java
@@ -1,8 +1,27 @@
-package com.que.service;
+package com.que.que.service;
 
-import org.springframework.stereotype.Service;
+import com.que.que.model.QueueUser;
+import java.util.PriorityQueue;
 
-@Service
 public class QueueService {
+    private final PriorityQueue<QueueUser> queue;
 
+    public QueueService() {
+        // Initialize the PriorityQueue with the custom comparator
+        this.queue = new PriorityQueue<>(QueueUser.PRIORITY_COMPARATOR);
+    }
+
+    // Method to add a user to the queue
+    public void joinQueue(QueueUser user) {
+        queue.add(user);
+    }
+
+    // Method to get the next user in the queue
+    public QueueUser callNext() {
+        return queue.poll();
+    }
+
+    public PriorityQueue<QueueUser> getAllUsers() {
+        return new PriorityQueue<>(queue);
+    }
 }
